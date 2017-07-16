0bcceabb3d03d50a33f7e100190d90a3cd9f03bc
Gatlingover <Samsonf21@gmail.com>
2017-07-16 15:31:20 +0100

    added new word dictionaries

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -55,13 +55,30 @@ HANGMANPICS = ['''
 /|\  |
 / \  |
      |
-=========''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar
-coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion
-lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon
-python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake
-spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat
-zebra'.split()
+=========''','''
+==========''', '''
++----+
+|    |
+[O   |
+/|\  |
+/ \  |
+     |
+==========''', '''
++----+
+|    |
+[O]  |
+/|\  |
+/ \  |
+     |
+==========''']
+AnimalWords = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+
+ColoursWords = 'red orange yellow green blue indigo violet white black brown'.split()
+
+ShapeWords = 'square triangle rectangle circle ellipse rhombus trapezoid chevron pentagon hexagon septagon octagon'.split()
+
+FruitWords = 'apple orange lemon lime pear watermelon grape grapefruit cherry banana cantaloupe mango strawberry tomato'.split()
+
 
 def getRandomword(wordList):
      # this function returns a random string from the passed list of strings.
